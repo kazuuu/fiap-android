0f1e6c2519bce9ef142913a41c25b0c94c4ff1ac
Marcello Kazuo <marcellokazuo@gmail.com>
2022-02-20 16:19:20 -0300

    username intent string

--- myapp/app/src/main/java/br/com/fiap/myapp/HomeActivity.kt
+++ myapp/app/src/main/java/br/com/fiap/myapp/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.ActionBar
 import java.util.*
 
@@ -11,6 +12,12 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val username = intent.getStringExtra("username")
+
+        val tv_username: TextView = findViewById(R.id.tv_username)
+        tv_username.text = username;
+
 
         //Start of dynamic title code---------------------
         val actionBar: ActionBar? = supportActionBar
