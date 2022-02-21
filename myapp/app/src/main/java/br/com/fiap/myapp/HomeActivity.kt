168eea1b50a2624e6ed362ddfc90a0f9e3e26949
Marcello Kazuo <marcellokazuo@gmail.com>
2022-02-20 22:16:23 -0300

    Action Bar Title change and comments

--- myapp/app/src/main/java/br/com/fiap/myapp/HomeActivity.kt
+++ myapp/app/src/main/java/br/com/fiap/myapp/HomeActivity.kt
@@ -13,20 +13,19 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        // Altera o texto da Action Bar
+        val actionBar: ActionBar? = getSupportActionBar()
+        if (actionBar != null)
+            actionBar.setTitle("Home")
+
+        // Pega o username que foi passado pela MainActivity
         val username = intent.getStringExtra("username")
 
+        // Coloca o username no TextView da HomeActivity
         val tv_username: TextView = findViewById(R.id.tv_username)
         tv_username.text = username;
 
-
-        //Start of dynamic title code---------------------
-        val actionBar: ActionBar? = supportActionBar
-        if (actionBar != null) {
-            //Setting a dynamic title at runtime. Here, it displays the current time.
-            actionBar.setTitle("Home")
-        }
-        //End of dynamic title code----------------------
-
+        // Configura o evento OnClick do botao Sair para executar a função cliqueSair()
         val btn_clique_me: Button = findViewById(R.id.btn_sair)
         btn_clique_me.setOnClickListener {
             cliqueSair()
@@ -34,6 +33,7 @@ class HomeActivity : AppCompatActivity() {
     }
 
     fun cliqueSair() {
+        // Vai para a tela da MainActivity e finaliza esta Activity.
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
